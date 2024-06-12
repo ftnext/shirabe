dc3ba49ba0241b8b253a12925c1cda4f24caca3b
ftnext <takuyafjp+develop@gmail.com>
2024-06-13 00:09:18 +0900

    [feat] Users can create venv with requirements

---
+++ src/shirabe/__main__.py
@@ -0,0 +1,3 @@
+from shirabe.venv import main
+
+main()
---
+++ src/shirabe/venv.py
@@ -0,0 +1,35 @@
+import os.path
+from venv import EnvBuilder
+
+
+class ShirabeEnvBuilder(EnvBuilder):
+    def post_setup(self, context):
+        env_dir = context.env_dir
+        requirements_file = os.path.join(
+            os.path.dirname(env_dir), "requirements.txt"
+        )
+        if os.path.exists(requirements_file):
+            self._call_new_python(
+                context,
+                "-m",
+                "pip",
+                "install",
+                "--no-deps",
+                "-r",
+                requirements_file,
+            )
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "dir",
+        metavar="ENV_DIR",
+        help="A directory to create the environment in.",
+    )
+    options = parser.parse_args()
+
+    builder = ShirabeEnvBuilder(with_pip=True)
+    builder.create(options.dir)
