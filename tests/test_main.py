5216a3205db2145045b027a5afe2206de1bcdb67
ftnext <takuyafjp+develop@gmail.com>
2024-06-20 22:51:49 +0900

    [bugfix] Run shirabe in test like pipx

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,12 @@
-import subprocess
+import sys
 from unittest.mock import patch
+
+from shirabe.venv import main
+
+
+def run_shirabe(venv_path):
+    with patch.object(sys, "argv", ["shirabe", venv_path]):
+        main()
 
 
 @patch("shirabe.venv.ShirabeEnvBuilder._install_requirements")
@@ -8,6 +15,6 @@ def test_python_m_shirabe_no_requirements(
 ):
     with monkeypatch.context() as m:
         m.chdir(tmp_path)
-        subprocess.run(["python", "-m", "shirabe", ".venv"])
+        run_shirabe(".venv")
 
     _install_requirements.assert_not_called()
